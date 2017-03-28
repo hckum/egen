1149a2248063951b19beeb8fc4ed9d263476295d
Han <whxjtu@gmail.com>
2017-03-28 16:13:15 -0500

    Second commit

--- gen.py
+++ gen.py
@@ -1,12 +1,15 @@
-# Error Generation Simulator v0.1
+# Functions for Error Generation Simulator v0.1
 # by Han Wang
-# 03/27/2017
+# 03/27/2017 first created, data preprocessing
+# 03/28/2017 add typo generation
 
 import csv
 import zipfile
 from datetime import date, datetime
 import calendar
 import random
+import string
+import copy
 
 
 def readzip(fpath):
@@ -19,23 +22,35 @@ def readzip(fpath):
     a = zipfile.ZipFile(fpath, 'r')
     b = a.open(fpath.split('/')[-1][:-4]+'.txt')
     reader = csv.reader(b, delimiter='\t')
+    title = []
     for i in reader:
-        if i[2]!='voter_reg_num':
-            d[i[2]] = [x.strip() for x in i] # [i[x].strip() for x in [9, 10, 25, 28]]
+        if i[2] == 'voter_reg_num':
+            title = [x.strip() for x in i]
+            break
+
+    for i in reader:
+        if i[2] != 'voter_reg_num':
+            d[i[2]] = {}
+            for n in range(len(i)):
+                d[i[2]][title[n]] = i[n].strip()
+
+            #d[i[2]] = [x.strip() for x in i] # [i[x].strip() for x in [9, 10, 25, 28]]
     return d
 
 
 def field_filter(data, fields):
     """
     Select columns as needed.
-    reg number: 2, last name: 9, first name: 10, birth age: 29
+    voter_reg_num, last_name, first_name, birth_age
     :param data: data dictionary
-    :param fields: list of selected
+    :param fields: list of selected fields
     :return: data only with selected fields
     """
     d = {}
     for k in data.keys():
-        d[k] = [data[k][x] for x in fields]
+        d[k] = {}
+        for f in fields:
+            d[k][f] = data[k][f]
     return d
 
 
@@ -45,14 +60,15 @@ def dob(record):
     :param record: a row of data
     :return: modified data by replacing age with dob.
     """
-    s = ' '.join(record)
+    tmp = record
+    s = ' '.join(record.values())
     ymd = date.fromordinal(sum([ord(x) for x in s])).isoformat()
-    year = 2017 - int(record[-1])
+    year = 2017 - int(record['birth_age'])
     # leap year 02-29 validation
     while '02-29' in ymd and not calendar.isleap(year):
         year -= 1
-    return record[:-1] + [ymd[5:] + '-' + str(year)]
-
+    tmp['dob'] = ymd[5:] + '-' + str(year)
+    return tmp
 
 def swap_fields(record):
     """
@@ -60,7 +76,11 @@ def swap_fields(record):
     :param record:
     :return: modified record
     """
-    return [record[i] for i in [0, 2, 1, 3]]
+    tmp = record
+    t = record['last_name']
+    tmp['last_name'] = record['first_name']
+    tmp['first_name'] = t
+    return tmp
 
 
 def swap_date(record):
@@ -69,14 +89,18 @@ def swap_date(record):
     :param record:
     :return: modified record
     """
-    k = record[-1]
+    tmp = record
+    k = record['dob']
     try:
         l = [i for i in k.split('-')]
         k = '-'.join([l[1],l[0],l[2]])
         datetime.strptime(k,'%m-%d-%Y')
+    except IndexError:
+        return tmp
     except ValueError:
-        return record
-    return record[:-1] + [k]
+        return tmp
+    tmp['dob'] = k
+    return tmp
 
 
 def del_field(record):
@@ -85,14 +109,174 @@ def del_field(record):
     :param record:
     :return: modified record
     """
-    f = random.randint(0,len(record))
-    return [record[i] if i != f else '' for i in range(len(record))]
+    tmp = record
+    f = random.randint(0,len(record.keys())-1)
+    tmp[tmp.keys()[f]] = ''
+    return tmp
 
 
-def match(x,y):
-    for j in range(len(x)):
-        if x[j]!=y[j]:
-            return False
+def insert(record):
+    """
+    Perform an insertion
+    :param record:
+    :return: modified record
+    """
+    tmp = record
+    pos = random.randint(0,len(tmp.keys())-1)
+    field = tmp[tmp.keys()[pos]]
+    if tmp.keys()[pos]=='dob':
+        # valid insertion: months: 2 -> 12, days: 5->25
+        mdy = tmp['dob'].split('-')
+        type = 0
+        if int(mdy[0])<=2:
+            type+=1
+        if int(mdy[1])<=9:
+            type+=2
+        if type == 0:
+            insert(record)
+        if type == 1:
+            mdy[0] = str(int(mdy[0])+10)
+        if type == 2:
+            mdy[1] = str(int(mdy[1])+random.randint(1,2)*10)
+        if type == 3:
+            t = random.randint(0,2)
+            if t==0:
+                mdy[0] = str(int(mdy[0])+10)
+            else:
+                mdy[1] = str(int(mdy[1])+t*10)
+        tmp['dob'] = '-'.join(mdy)
+    elif field.isdigit():
+        # voter_reg_num insertion
+        p = random.randint(0, len(field))
+        if p == len(field):
+            tmp[tmp.keys()[pos]] += string.digits[random.randint(0,9)]
+        else:
+            tmp[tmp.keys()[pos]] = field[:p]+string.digits[random.randint(0,9)]+field[p:]
+    elif field.isalpha():
+        # last_name and first_name insertion
+        p = random.randint(0,len(field))
+        if p == len(field):
+            tmp[tmp.keys()[pos]] += string.uppercase[random.randint(0,25)]
+        else:
+            tmp[tmp.keys()[pos]] = field[:p]+string.uppercase[random.randint(0,25)]+field[p:]
+    return tmp
+
+
+def delete(record):
+    """
+    Perform a deletion
+    :param record:
+    :return: modified record
+    """
+    tmp = record
+    pos = random.randint(0, len(tmp.keys()) - 1)
+    field = tmp[tmp.keys()[pos]]
+    if tmp.keys()[pos]=='dob':
+        # valid deletion: 21->01, 21->02, 20->02
+        mdy = tmp['dob'].split('-')
+        t = random.randint(0,1)
+        if int(mdy[t])>=10:
+            mdy[t] = '0'+mdy[t][0] if int(mdy[t])%10==0 else '0'+mdy[t][random.randint(0,1)]
+        tmp['dob'] = '-'.join(mdy)
+    elif field!='':
+        p = random.randint(0,len(field)-1)
+        tmp[tmp.keys()[pos]] = field[:p]+field[p+1:]
+    return tmp
+
+
+def transpose(record):
+    """
+    Perform a transposition
+    :param record:
+    :return: modified record
+    """
+    tmp = record
+    pos = random.randint(0, len(tmp.keys()) - 1)
+    field = tmp[tmp.keys()[pos]]
+    if tmp.keys()[pos]=='dob':
+        pass
+    else:
+        p = random.randint(0, len(field)-2)
+        tmp[tmp.keys()[pos]] = field[:p]+field[p+1]+field[p]+field[p+2:]
+    return tmp
+
+
+def substitute(record):
+    """
+    Perform a substitution
+    :param record:
+    :return: modified record
+    """
+    tmp = record
+    pos = random.randint(0, len(tmp.keys()) - 1)
+    field = tmp[tmp.keys()[pos]]
+    if field!='':
+        p = random.randint(0,len(field)-1)
+        if tmp.keys()[pos]=='dob':
+            pass
+        elif field.isdigit():
+            tmp[tmp.keys()[pos]] = tmp[tmp.keys()[pos]].replace(tmp[tmp.keys()[pos]][p], string.digits[random.randint(0,9)])
+        elif field.isalpha():
+            tmp[tmp.keys()[pos]] = tmp[tmp.keys()[pos]].replace(tmp[tmp.keys()[pos]][p], string.uppercase[random.randint(0,25)])
+    return tmp
+
+
+def add_typo(record, prob):
+    """
+    Add a typo error:
+    1. insertion
+    2. deletion (omission)
+    3. transpose
+    4. substitution (replace)
+    :param record: a record
+    :param prob: probabilities of each typo type (should be a list of 4 decimals)
+    :return: modified record
+    """
+    # prob normalization
+    p = [x/float(sum(prob)) for x in prob]
+    # create intervals
+    q = [sum(p[:x]) for x in range(1, len(p)+1)]
+
+    tmp = copy.deepcopy(record)
+    k = random.random()
+    # insertion
+    if k<=q[0]:
+        print '===Insertion==='
+        tmp = insert(tmp)
+    # deletion
+    elif k<=q[1]:
+        print '===Deletion==='
+        tmp = delete(tmp)
+    # transpose
+    elif k<=q[2]:
+        print '===Transposition==='
+        tmp = transpose(tmp)
+    # substitution
+    elif k<=q[3]:
+        print '===Substitution==='
+        tmp = substitute(tmp)
+    if tmp==record:
+        add_typo(record, prob)
+    else:
+        print tmp.values(), record.values()
+        return tmp
+
+#def name_variant(record):
+
+
+def match(x, y):
+    """
+    Match test
+    :param x: record x
+    :param y: record y
+    :return: True if they match, otherwise false
+    """
+    try:
+        for j in x.keys():
+            if x[j] != y[j]:
+                return False
+    except AttributeError:
+        pass
     return True
 
 
--- main.py
+++ main.py
@@ -1,58 +1,59 @@
-# Error Generation Simulator
+# Error Generation Simulator v0.1
 # by Han Wang
-#
+# 03/28/2017
+
 from gen import *
 import random
+import csv
+from copy import deepcopy
 
+filter1 = ['voter_reg_num', 'last_name', 'first_name', 'birth_age']
+filter2 = ['voter_reg_num', 'last_name', 'first_name', 'dob']
 
 p = readzip('data/apr13/ncvoter100.zip')
-dat = field_filter(p, [2, 9, 10, 29])
-
-rate = 0.03
-rate_missing = 0.5
-rate_swap = 0.5
-n = int(rate*len(dat.keys()))
-d = {}
-
+dat = field_filter(p, filter1)
 for k in dat.keys():
     dat[k] = dob(dat[k])
+data = deepcopy(dat)
+
+
+rate = 0.03
+rate_missing = 0.3
+rate_swap = 0.2
+rate_typo = 0.5
+typo_prob = [0.4, 0.3, 0.15, 0.15]
+n = int(rate*len(dat.keys()))
+d = {}
+op = [rate_missing, rate_swap, rate_typo]
+q = [sum(op[:x]) for x in range(1, len(op) + 1)]
 
 while len(d.keys()) <= n:
-    t = random.randint(0,len(dat.keys()))
-    k = dat.keys()[t]
-    if random.random() < rate_missing:
-        tmp = del_field(dat[k])
-    elif match(swap_date(dat[k]), dat[k]):
-        tmp = swap_fields(dat[k])
-    else:
-        tmp = swap_date(dat[k])
+    t = random.randint(0,len(data.keys())-1)
+    k = data.keys()[t]
+    p = random.random()
+    if p < q[0]:
+        tmp = del_field(data[k])
+    elif p < q[1]:
+        if match(swap_date(data[k]), data[k]):
+            tmp = swap_fields(data[k])
+        else:
+            tmp = swap_date(data[k])
+    elif p < q[2]:
+        tmp = add_typo(data[k], typo_prob)
     if not match(tmp,dat[k]):
         d[k] = tmp
 
 for k in d.keys():
-    print d[k],dat[k]
-print float(len(d.keys()))/len(dat.keys())
+    data[k] = d[k]
 
+with open('log/dict1.txt', 'wb') as f:
+    w = csv.DictWriter(f, filter2, extrasaction='ignore')
+    w.writeheader()
+    for k in sorted(dat.keys(),key=lambda x:int(x)):
+        w.writerow(dat[k])
 
-
-fname = {}
-lname = {}
-for k in dat.keys():
-    if dat[k][1] not in lname:
-        lname[dat[k][1]] = 1
-    else:
-        lname[dat[k][1]] += 1
-    if dat[k][2] not in fname:
-        fname[dat[k][2]] = 1
-    else:
-        fname[dat[k][2]] += 1
-
-with open('tmp3.txt','wb') as f:
-    c = [[k,lname[k]] for k in lname.keys()]
-    w = csv.writer(f)
-    w.writerows(sorted(c, key=lambda x:x[0]))
-
-with open('tmp4.txt','wb') as f:
-    c = [[k,fname[k]] for k in fname.keys()]
-    w = csv.writer(f)
-    w.writerows(sorted(c, key=lambda x:x[0]))
+with open('log/dict2.txt', 'wb') as f:
+    w = csv.DictWriter(f, filter2, extrasaction='ignore')
+    w.writeheader()
+    for k in sorted(data.keys(), key=lambda x:int(x)):
+        w.writerow(data[k])
