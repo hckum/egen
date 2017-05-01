ab8c805bfd8b2054086e9af2eb7f7423f0b9fbba
Han <whxjtu@gmail.com>
2017-05-01 15:18:23 -0500

    Refactoring. Ready to release.

--- main.py
+++ main.py
@@ -2,8 +2,14 @@
 # by Han Wang
 # 03/28/2017
 
-from egs import egs
+from core.table import Table
 
 if __name__ == "__main__":
-    config = 'config.txt'
-    egs(config)
+    t = Table()
+    t.load_config('config.txt')
+    t.load_data('data/apr13.csv')
+    #print [x[0] for x in t.config['fields_formats']]
+    #print t.get_original_data()[0]
+    #print t.formats
+    t.generate()
+    t.write()
