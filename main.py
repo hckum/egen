5bb000ccc979a8d67c820c7062d9ff7023545622
Han <whxjtu@gmail.com>
2017-05-02 13:35:13 -0500

    travis

--- main.py
+++ main.py
@@ -8,8 +8,5 @@ if __name__ == "__main__":
     t = Table()
     t.load_config('config.txt')
     t.load_data('data/apr13.csv')
-    #print [x[0] for x in t.config['fields_formats']]
-    #print t.get_original_data()[0]
-    #print t.formats
     t.generate()
     t.write()
